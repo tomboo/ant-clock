7847735ccaeea13296153c42813b05fbd5e19942
Tom Booster <tom.booster@gmail.com>
2020-02-17 04:05:16 -0800

    Update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,47 +1,63 @@
 import React from 'react';
 import 'antd/dist/antd.css';
 import './index.css';
-import { Button, Progress } from 'antd';
-import { clockify, fromMin, toMin, toDate } from "../utilities";
+import { Button, Progress, TimePicker } from 'antd';
 import { useState } from "react";
 import { useInterval } from "../utilities/hooks";
+import moment from 'moment';
 
 const STATE_INIT = "initial";
 const STATE_RUN = "run";
 const STATE_PAUSE = "pause";
 
-const INTERVAL = 1000; // update interval (1 sec)
+const INTERVAL = 500; // update interval (500 msec)
 
+// Convert moment.duration to ms
+function totalMS(d) {
+  return (
+    ((d.hours() * 60
+    + d.minutes()) * 60
+    + d.seconds()) * 1000
+    + d.milliseconds()
+  );
+}
+
+// Timer Component
 function Timer(props) {
+  // timer state
   const [timerState, setTimerState] = useState(STATE_INIT);
 
   // current interval
-  const [duration, setDuration] = useState(fromMin(1));
-  const [remaining, setRemaining] = useState(fromMin(1));
-  const [start, setStart] = useState(0);
-  const [end, setEnd] = useState(0);
+  const [duration, setDuration] = useState(10000);  // duration (ms)
+  const [remaining, setRemaining] = useState(duration);    // duration (ms)
+  const [start, setStart] = useState(0);            // time (ms)
+  const [end, setEnd] = useState(0);                // time (ms)
   
-  const [delay, setDelay] = useState(null);
+  // update interval
+  const [delay, setDelay] = useState(null);         // update interval (ms)
   useInterval(() => {
     tick();
   }, delay);
 
-  // Handle click on IntegerControl increment/decrement buttons
-  function setLength(increment) {
-    if (timerState !== STATE_INIT) return;
+  // event handlers
+  function onDurationChange(time) {
+    const ms = totalMS(time);
+    setDuration(ms);
+    setRemaining(ms);
+  }
 
-    let newLength = toMin(duration) + increment;
-    if (0 < newLength && newLength <= 60) {
-      newLength = fromMin(newLength);
-
-      setDuration(newLength);
-      setRemaining(newLength);
+  function progressText() {
+    if (timerState !== STATE_INIT) {
+      return moment(remaining).utc().format('HH:mm:ss')
+    }
+    else {
+      return 'Done';
     }
   }
 
   function onCancel() {
     stopTimer();
-    setRemaining(0);
+    setRemaining(duration);
     setStart(0);
     setEnd(0);
     setTimerState(STATE_INIT);
@@ -69,20 +85,23 @@ function Timer(props) {
   }
 
   function startTimer() {
-    console.log("startTimer", toDate(Date.now()));
     setDelay(INTERVAL);
   }
 
   function stopTimer() {
-    console.log("stopTimer", toDate(Date.now()));
     setDelay(null);
   }
 
   function tick() {
     const now = Date.now();
     setRemaining(Math.max(0, end - now));
-    console.log("tick ", "now: ", toDate(now), ", remaining: ", clockify(remaining, 1));
-    console.log("start: ", toDate(start), ", end: ", toDate(end));
+
+    console.log("tick ");
+    console.log("now: ", now);
+    console.log("remaining: ", remaining);
+    console.log("start: ", start);
+    console.log("end: ", end);
+    console.log("duration: ", duration);
 
     // on update remaining time
     /*
@@ -90,25 +109,41 @@ function Timer(props) {
       this.tickAlarm(remaining);
       this.tickPhase(remaining);
     */
-    if (remaining === 0) {
+    if (remaining <= 0) {
       stopTimer();
       setTimerState(STATE_INIT);
+      setRemaining(duration);
 
       console.log("*** Play Audio ***");
     }
   }
 
-  const percent = duration ? Math.floor((remaining / duration) * 100) : 0;
-  
+  const percentRemaining = (remaining && duration)
+    ? Math.floor(remaining / duration * 100)
+    : 0;
+  const durationMoment = moment(duration).utc();
+  console.log('duration:', duration);
+
   return (
     <div>
       <h1>Timer</h1>
 
       <div>
+          {/* defaultValue={moment('00:05:00', 'HH:mm:ss')} */}
+          <TimePicker
+            defaultValue={durationMoment}
+            onChange={(time) => onDurationChange(time)}
+            size="large"
+            secondStep={10}
+            disabled={timerState !== STATE_INIT}
+          />
+      </div>
+
+      <div>
         <Progress
           type="circle"
-          percent={percent}
-          format={percent => clockify(remaining)}
+          percent={percentRemaining}
+          format={() => progressText()}
         />
       </div>
 
@@ -133,7 +168,6 @@ function Timer(props) {
           </Button>
         )}
       </div>
-
     </div>
   );
 }
