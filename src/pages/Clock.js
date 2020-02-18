dd2491a021d29cc0be0e04d3b97da506f3155666
Tom Booster <tom.booster@gmail.com>
2020-02-17 20:05:19 -0800

    Add Clock component

--- src/pages/Clock.js
+++ src/pages/Clock.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import { useState } from "react";
+import { useInterval } from "../utilities/hooks";
 
+// Clock Component
 function Clock(props) {
+  const [currentTime, setCurrentTime] = useState(Date.now());
+  const [delay, setDelay] = useState(500);
+  useInterval(() => {
+    setCurrentTime(Date.now());
+  }, delay);
+
+  const date = new Date(currentTime);
+
   return (
-    <h1>Clock</h1>
+    <div className="Clock">
+      <div className="Label">
+        {date.toLocaleDateString(undefined, { weekday: "long" })}
+      </div>
+      <div className="Label">
+        {date.toLocaleDateString(undefined, {
+          month: "long",
+          day: "numeric",
+          year: "numeric"
+        })}
+      </div>
+      <div className="Label">{date.toLocaleTimeString()}</div>
+    </div>
   );
 }
 
